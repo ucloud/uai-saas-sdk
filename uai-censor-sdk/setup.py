ca17e1893ec99a3e2c7362c21f3d514c30cd7a21
Flora <flora@FloradeMacBook-Air.local>
2019-04-24 15:26:46 +0800

    fix dependency

--- uai-censor-sdk/setup.py
+++ uai-censor-sdk/setup.py
@@ -50,5 +50,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    install_requires=['requests==2.10.0'],
+    install_requires=['requests==2.20.1'],
 )
